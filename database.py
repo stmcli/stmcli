ba77d0c4cf0feba0994e74de9d9caea1a8cf9d2d
pgdagenais <pgdagenais@gmail.com>
2016-03-12 00:40:48 -0500

    Added database insert from the stm files

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+import os
 import sqlite3
+import unicodecsv
 
 
 def init_table():
@@ -11,6 +12,7 @@ def init_table():
                  agency_url char(60) not null,
                  agency_timezone char(20) not null,
                  agency_lang char(10),
+                 agency_phone char(20),
                  agency_fare_url char(255)
                  );''')
     conn.execute('''CREATE TABLE stops
@@ -37,7 +39,8 @@ def init_table():
                  service_id int not null,
                  trip_id char(20) not null,
                  trip_headsign char(50),
-                 wheelchair_accessible boolean,
+                 direction_id boolean,
+                 wheelchair_accessible int,
                  shape_id char(15),
                  note_fr char(255),
                  note_en char(255)
@@ -53,3 +56,48 @@ def init_table():
                  (download_date date
                  );''')
     conn.close()
+
+
+def load_data(data_file):
+    conn = sqlite3.connect('stm.db')
+    cursor = conn.cursor()
+
+    file_path = 'stm/' + data_file
+    with open(file_path, 'rb') as input_file:
+        reader = unicodecsv.reader(input_file, delimiter=",")
+        data = [row for row in reader]
+
+    if "agency" in data_file:
+        print("agency")
+        cursor.executemany('''INSERT INTO agency
+                           VALUES (?, ?, ?, ?, ?, ?, ?);''', data)
+    elif "stops" in data_file:
+        print("stops")
+        cursor.executemany('''INSERT INTO stops
+                           VALUES (?, ?, ?, ?, ?, ?, ?);''', data)
+    elif "routes" in data_file:
+        print("routes")
+        cursor.executemany('''INSERT INTO routes
+                           VALUES (?, ?, ?, ?, ?, ?, ?, ?);''', data)
+    elif "trips" in data_file:
+        print("trips")
+        cursor.executemany('''INSERT INTO trips
+                           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);''', data)
+    elif "stop_times" in data_file:
+        print("stop_times")
+        cursor.executemany('''INSERT INTO stop_times
+                           VALUES (?, ?, ?, ?, ?);''', data)
+    else:
+        print("There is not table for " + data_file)
+    conn.commit()
+
+
+def main():
+    init_table()
+
+    stm_file_dir = os.listdir('stm')
+    for filename in stm_file_dir:
+        load_data(filename)
+
+
+main()
