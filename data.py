e0981631ceffb43c6f26f3ef6bea200a6a47209b
lacsap- <lacsap@cock.li>
2016-03-12 14:56:56 -0500

    zip file is now extracted to stm/ and deleted

--- data.py
+++ data.py
@@ -5,9 +5,10 @@ import sqlite3
 import time
 import urllib
 from urllib import error, request
+import zipfile
 
 
-def download_gtfs_zip():
+def download_gtfs_data():
     try:
         output_dir = os.path.dirname(os.path.realpath(__file__)) + "/gtfs.zip"
         zip_url = "http://www.stm.info/sites/default/files/gtfs/gtfs_stm.zip"
@@ -17,6 +18,17 @@ def download_gtfs_zip():
     except urllib.error.HTTPError as err:
         print("Error {0} while trying to downloads stm infos")
         exit(1)
+
+    # Extracting to stm/ and deleting zip
+    extract_location = "stm/"
+
+    if not os.path.isdir(extract_location):
+        os.makedirs(extract_location)
+
+    zip = zipfile.ZipFile('gtfs.zip')
+    zip.extractall(path=extract_location)
+
+    os.unlink("gtfs.zip")
 
 
 def check_for_update():  # Return True if update is needed, False if not
