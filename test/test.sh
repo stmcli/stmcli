7b018c2ed9037c1cd6ecdd2aded0247449676d1e
lacsap- <lacsap@cock.li>
2016-03-30 19:14:49 -0400

    Updated tests

--- test/test.sh
+++ test/test.sh
@@ -8,18 +8,37 @@ echo "database.init_table()"
 echo "database.load_stm_data()"
 echo "database.load_data()"
 stmcli -y
+if [[ $? -ne 0 ]]
+then
+  echo "Update Failed"
+fi
 
-
-echo "printinfo.next_departures()"
+echo "---------printinfo.next_departures()---------"
 stmcli -s 51253 -b 435
+if [[ $? -ne 0 ]]
+then
+  echo "printinfo.next_departures() Failed"
+fi
 
-echo "main() (custom time test)"
+echo "---------main() (custom time test)---------"
 stmcli -s 51253 -b 435 -t 10:30
+if [[ $? -ne 0 ]]
+then
+  echo "main() (custom time test) Failed"
+fi
 
-echo "printinfo.all_bus_for_stop_code()"
+echo "---------printinfo.all_bus_for_stop_code()---------"
 stmcli -s 51253
+if [[ $? -ne 0 ]]
+then
+  echo "printinfo.all_bus_for_stop_code() Failed"
+fi
 
-echo "printinfo.all_bus_stop()"
+echo "---------printinfo.all_bus_stop()---------"
 stmcli -b 435
+if [[ $? -ne 0 ]]
+then
+  echo "printinfo.all_bus_stop() Failed"
+fi
 
 echo "Not tested yet : data.date_in_scope()"
